23934ed61c624e9afdd2490cd3a9a63803c8070a
laixi74 <78567259+laixi74@users.noreply.github.com>
2021-03-30 09:44:28 +1300

    Changed the mistake in the components for other pages.

--- src/routes/visual-communication.svelte
+++ src/routes/visual-communication.svelte
@@ -1,5 +1,5 @@
 <script>
     import Header from "$lib/Header.svelte";
-    import Nav from "lib/Nav.svelte";
-    import Footer from "lib/Footer.svelte";
+    import Nav from "$lib/Nav.svelte";
+    import Footer from "$lib/Footer.svelte";
 </script>
