5d07159142fcb931f680a48e4501118e30f99978
laixi74 <78567259+laixi74@users.noreply.github.com>
2021-03-30 09:43:47 +1300

    tested whether the imports of components worked.
    Tested in safari, it didn't work first. I went and read through and found that I was missing the $ sign in the import section and that was why it failed to show up in safari.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,11 @@
 <script>
     import Header from "$lib/Header.svelte";
-    import Nav from "lib/Nav.svelte";
-    import Footer from "lib/Footer.svelte";
+    import Nav from "$lib/Nav.svelte";
+    import Footer from "$lib/Footer.svelte";
 </script>
+
+<Header />
+<Nav />
+
+<Footer />
 
