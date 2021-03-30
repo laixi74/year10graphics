fecd26dacef85fadf0ffda8c6e80ccbfbae36922
laixi74 <78567259+laixi74@users.noreply.github.com>
2021-03-30 14:39:26 +1300

    Style the nav using bulma.
    Tested in safari, but the nav menu wasn't what I expected, it had the home on one line and the other on another line. So I added the link to bulma, and this time it appeared the way I wanted.

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,5 +1,9 @@
-<nav>
-    <a href=".">Home</a>
-    <a href="design-heritage">Design Heritage</a>
-    <a href="visual-communication">Visual Communication</a>
+<nav class="navbar">
+    <div class="navbar-brand">
+        <a class="navbar-item" href=".">Home</a>
+    </div>
+    <div class="navbar-menu">
+    <a class="navbar-item" href="design-heritage">Design Heritage</a>
+    <a class="navbar-item" href="visual-communication">Visual Communication</a>
+    </div>
 </nav>
